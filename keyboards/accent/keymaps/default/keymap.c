257c1078827fee596ba2bee5ded7aa3f64f1cf25
taku25 <taku36@gmail.com>
2019-12-29 15:31:34 +0900

    bs to delete

--- keyboards/accent/keymaps/default/keymap.c
+++ keyboards/accent/keymaps/default/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+------+----- +------+------+-------|                      |------+------+----- +------+------+----------|
  * |LCTRL(`) |   A  |   S  |   D  |   F  |   G   |                      |  h   |  j   |  k   |  l   |  ;   | RCTRL([) |
  * |---------+------+------+------+------+-------|-------.      .-------|------+------+------+------+------+----------|
- * |RAISE(`) |   Z  |   X  |   C  |   V  |   B   |  BSSP |      | esc   |  n   |  m   |  ,   |  .   |  /   | RAISE(]) |
+ * |RAISE(`) |   Z  |   X  |   C  |   V  |   B   |  Dele |      | esc   |  n   |  m   |  ,   |  .   |  /   | RAISE(]) |
  * `---------+------+------+------+------+-------+-------|      |-------+------+------+------+------+------|----------'
  *           |  &   | LGUI | ALT  |      | LShift| Spacet|      | enter |RShift|      | ALT  | GUI  |  \   |
  *           |      |      |      | LOW  | lagn2 |       |      |       |lagn1 | LOW  |      |      |      |
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT( \
   KC_MINUS,                   KC_Q,      KC_W,    KC_E,    KC_R,              KC_T,                                    KC_Y,    KC_U,     KC_I,    KC_O,       KC_P,           KC_EQUAL,\
   LCTL_T(KC_QUOTE),           KC_A,     KC_S,     KC_D,    KC_F,              KC_G,                                    KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN,    RCTL_T(KC_LBRC),\
-  LT(_RAISE, KC_GRV),         KC_Z,     KC_X,     KC_C,    KC_V,              KC_B,   KC_BSPC,    KC_ESC,              KC_N,    KC_M,  KC_COMM,  KC_DOT,    KC_SLSH, LT(_RAISE,KC_RBRC),\
+  LT(_RAISE, KC_GRV),         KC_Z,     KC_X,     KC_C,    KC_V,              KC_B,   KC_DELETE,    KC_ESC,              KC_N,    KC_M,  KC_COMM,  KC_DOT,    KC_SLSH, LT(_RAISE,KC_RBRC),\
                       KC_AMPERSAND,  KC_LGUI,  KC_LALT,   LOWER,  LSFT_T(KC_LANG2),    KC_SPC,    KC_ENT,  RSFT_T(KC_LANG1),   LOWER,  KC_RALT, KC_RGUI,  KC_BSLASH\
 ),
 /* LOWER
