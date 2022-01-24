f1110cbf4a9d3bf2576eb3189f7104e51070654f
taku25 <taku36@gmail.com>
2022-01-24 10:25:42 +0900

    fix product id

--- keyboards/accent/glissando/rev1/config.h
+++ keyboards/accent/glissando/rev1/config.h
@@ -18,13 +18,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0x6B62
-#define PRODUCT_ID      0x0003
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    taku25
-#define PRODUCT         Accent_glissando_rev1
-
 // #define TAPPING_FORCE_HOLD
 // #define TAPPING_TERM 100
 
